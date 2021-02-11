4f817ac79303daec2b778342f29979c518952c4e
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-02-11 18:43:23 +0800

    cleaned up

--- mycobot/firmware/remapper/src/firmware.ino
+++ mycobot/firmware/remapper/src/firmware.ino
@@ -1,9 +1,11 @@
 #define SERVO_PIN 3
 #define SENSOR_PIN 2
 #define LED_PIN 1
-long pulse = 0;
 
-void setup() {
+long g_pulse = 0;
+
+void setup() 
+{
     OCR0A = 0xAF;            
     TIMSK |= _BV(OCIE0A);
 
@@ -12,20 +14,21 @@ void setup() {
     pinMode(SENSOR_PIN, INPUT);
 }
 
-void loop() {
+void loop() 
+{
     unsigned long pwm_val = pulseIn(SENSOR_PIN, HIGH, 20000);
-    pulse = map(pwm_val, 0, 500, 2450, 550);
+    g_pulse = map(pwm_val, 0, 500, 2450, 550);
     delay(15); 
 }
 
 volatile uint8_t counter = 0;
 SIGNAL(TIMER0_COMPA_vect) 
 {
-  counter += 2;
-  if (counter >= 20) {
-    counter = 0;
-    digitalWrite(SERVO_PIN, HIGH);
-    delayMicroseconds(pulse);
-    digitalWrite(SERVO_PIN, LOW);
-  }
+	counter += 2;
+	if (counter >= 20) {
+		counter = 0;
+		digitalWrite(SERVO_PIN, HIGH);
+		delayMicroseconds(g_pulse);
+		digitalWrite(SERVO_PIN, LOW);
+	}
 }
