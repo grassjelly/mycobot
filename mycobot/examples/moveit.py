6d6cf8d164dc95e9b7875fa60e35053a9b87c25b
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-02-11 19:45:28 +0800

    added initial position

--- mycobot/examples/moveit.py
+++ mycobot/examples/moveit.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     rospy.init_node("moveit_test", anonymous = True, disable_signals=True)
     
     manipulator = MyCobotCommander()
+    init_joints = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    manipulator.move_joints(init_joints)
+
     time.sleep(1)
     manipulator.open_gripper()
     time.sleep(1)
@@ -16,16 +19,24 @@ if __name__ == "__main__":
     time.sleep(1)
     manipulator.open_gripper()
     time.sleep(1)
+
     while not rospy.is_shutdown():
-        target_x = 0.2
+        target_x = 0.22
         target_y = 0.0
-        target_z = 0.055
+        target_z = 0.10
         manipulator.close_gripper()
         time.sleep(1)
 
         manipulator.move_to_goal_pose(target_x, 
                                       target_y, 
                                       target_z, 
+                                      roll=0.0,
+                                      pitch=1.5708,
+                                      yaw=0.0)
+
+        manipulator.move_to_goal_pose(target_x, 
+                                      target_y, 
+                                      target_z-0.05, 
                                       roll=0.0,
                                       pitch=1.5708,
                                       yaw=0.0)
